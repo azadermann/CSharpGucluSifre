a2c73a418fb1d6e2f6c41f3b3f2d6c99c9e6260d
azadermann <azaderman7@gmail.com>
2022-11-08 01:11:45 +0300

    Proje dosyası ekle.

---
+++ Guclu_Sifre/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+
+internal class Program
+{
+    public static string GuclluSifre(int lenght)
+    {
+        const string chars = "0123456789qwertyuıopğüasdfghjklşizxcvbnmöçQWERTYUIOPĞÜASDFGHJKLŞİZXCVBNMÖÇ";
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random();
+
+        for (int i = 0; i < lenght; i++)
+        {
+            int indeks = rnd.Next(chars.Length);
+            sb.Append(chars[indeks]);
+        }
+        return sb.ToString();
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Bir Şifre Uzunluğunu Giriniz Ör: 12 ==> ");
+        int lnght = Convert.ToInt32(Console.ReadLine());
+
+        string sifre = GuclluSifre(lnght);
+
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("                                                Yeni Şifre Güçlü Şifre                               ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(sifre);
+        
+
+        Console.ReadLine();
+        
+
+
+
+    }
+}
